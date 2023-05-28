b0f18d4cacf3b18b22e13711f9ca8fbc2b7d151c
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-29 02:13:40 +0300

    feat: add hero section

---
+++ src/components/home/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='lg:h-[85vh] flex items-center bg-primary lg:bg-cover
+    lg:bg-no-repeat py-32 lg:py-0 overflow-hidden'>
+        <div className='container mx-auto'>
+            Hero
+        </div>
+    </section>
+  )
+}
+
+export default Hero
