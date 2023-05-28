b8cfa427db4ebc582efa052088e03fde5968c0d4
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-29 02:13:19 +0300

    feat: navbar section model

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react'
+import Logo from './Logo';
+import Socials from './Socials';
+import Mobile from './Mobile';
+import '../style.css';
+
+function Navbar() {
+  const [bg, setBg] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', ()=> {
+      return window.scrollY > 50? setBg(true) : setBg(false);
+    })
+  },[])
+  return (
+    <header className={`${bg ? 'bg-tertiary h-20' : 'h-24'}
+    flex items-center fixed top-0 w-full text-white z-10 transition-all duration-300 
+    `}>
+      <div className='container mx-auto h-full flex items-center justify between'> 
+       <a href='#'>Barbra Mududa </a> 
+      </div>
+      <div className='hidden lg:block'>
+        <Logo />
+      </div>
+      <div className='hidden lg:block'>
+        <Socials />
+      </div>
+      <div className='lg:hidden'>
+        <Mobile />
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
