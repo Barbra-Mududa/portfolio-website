8dedc8fb5f183b5b5223993d625c65cc302b7a77
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-05-31 02:35:06 +0300

    feat: include new sections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/home/Hero';
-
-// import components
+import About from './components/home/About';
+import Skills from './components/home/Skills';
 
 const App = () => {
   return <div>
     <Navbar />
     <Hero />
+    <About />
+    <Skills />
     <div style={{height: '2000px'}}></div>
   </div>;
 };
