1af7df3b0689504c744303e301d3d57590ae7c84
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-06-01 09:54:50 +0300

    refactor: adjust component strucutre

--- src/components/projects/PSection.js
+++ src/components/projects/PSection.js
@@ -3,14 +3,14 @@ import Projects from './Projects'
 
 const PSection = () => {
   return (
-    <section id='main' className='section bg-primary min-h-[1400px]'>
+    <section id='main' className='section bg-primary '>
       <div className='container mx-auto'>
         <div className='flex flex-col  items-center text-center'>
           <h2 className='section-title before:content-experience relative before:absolute before:opacity-40 
-          before:-top-[2rem] before:-left-64 before:hidden before:lg:block'>
+          before:-top-[1rem] before:-left-64 before:hidden before:lg:block text-6xl'>
             My Projects
           </h2>
-          <p className='sub-title'>I am thrilled to showcase a blend of my individual strengths and remarkable collaborative ventures with other skilled developers.</p>
+          <p className='subtitle text-2xl '>I am thrilled to showcase a blend of my individual strengths and remarkable collaborative ventures with other skilled developers.</p>
         </div>
         <Projects />
       </div>
