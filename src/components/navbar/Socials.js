d714ed17157985ff04e42e52f317eee5dae35c19
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-06-01 23:33:38 +0300

    feat: add fixed position,  hover effect

--- src/components/navbar/Socials.js
+++ src/components/navbar/Socials.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { social } from '../../data'
+import React from 'react';
+import { social } from '../../data';
 
 function Socials() {
   return (
-    <ul className='flex space-x-6'>
+    <ul className='fixed left-0 top-0 h-full flex flex-col justify-center items-center space-y-6'>
       {social.map((item, index) => {
-            return(
-              <li className='flex justify-center items-center text-accent' key={index}>
-                <a className='text-base' href={item.href}>
-                  {item.icon}
-                </a>
-              </li>
-            )
-        })}           
+        return (
+          <li className='flex justify-center items-center text-white' key={index}>
+            <a
+              className='text-4xl transition duration-300 ease-in-out hover:text-accent'
+              href={item.href}
+            >
+              {item.icon}
+            </a>
+          </li>
+        );
+      })}
     </ul>
-  )
+  );
 }
 
-export default Socials
+export default Socials;
