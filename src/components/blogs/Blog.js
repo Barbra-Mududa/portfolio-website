fdc9be5cd550f70e8c8922d0ad7fc3e3f8bda1bd
Barbra Mududa <barbra.mududa@student.moringaschool.com>
2023-06-01 23:30:13 +0300

    feat:add blog component structure

--- src/components/blogs/Blog.js
+++ src/components/blogs/Blog.js
@@ -1,9 +1,34 @@
 import React from 'react'
-import BlogCards from './BlogCards'
+import { blogs } from '../../data'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <section id='blogs' className='section bg-tertiary'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col  items-center text-center'>
+          <h3 className='section-title before:content-blogs relative before:absolute before:opacity-40 
+          before:-top-[1rem] before:-left-28 before:hidden before:lg:block text-6xl'>
+            My Insights
+          </h3>
+        </div>
+        <div className="grid lg:grid-cols-3 gap-8">
+          {blogs.map((blog, index) => {
+          const { blogImg, blogName, blogText } = blog;
+          return (
+            <div className="bg-secondary p-6 rounded-3xl" key={index}>
+              <div className="card relative group">
+                <img className="card-img" src={blogImg} alt={blogName} />
+                <div className="card-content">
+                  <h3 className="card-title">{blogName}</h3>
+                  <p className="card-text">{blogText}</p>
+                </div>
+              </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   )
 }
 
